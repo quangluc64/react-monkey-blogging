91510de6f0a84dcab2039ed01670e48abddff7af
quangluc64 <taquangluc10b3@gmail.com>
2025-07-26 22:45:38 +0700

    Chức năng thêm Danh mục phần 2

--- src/module/category/CategoryAddNew.js
+++ src/module/category/CategoryAddNew.js
@@ -1,13 +1,21 @@
 import { Button } from "components/button";
-import { Checkbox, Radio } from "components/checkbox";
+import { Radio } from "components/checkbox";
 import { Field } from "components/field";
 import { Input } from "components/input";
 import { Label } from "components/label";
+import { db } from "firebase-app/firebase-config";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import DashboardHeading from "module/dashboard/DashboardHeading";
 import React from "react";
 import { useForm } from "react-hook-form";
+import slugify from "react-slugify";
+import { toast } from "react-toastify";
+import { categoryStatus } from "utils/constants";
 const CategoryAddNew = () => {
-  const { control, handleSubmit } = useForm({
+  const {
+    control, watch, reset, handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm({
     mode: "onChange",
     defaultValues: {
       name: "",
@@ -16,8 +24,28 @@ const CategoryAddNew = () => {
       createdAt: new Date(),
     },
   });
-  const handleAddNewCategory = (values) => {
-    console.log("values ~", values);
+  const watchStatus = watch("status");
+  const handleAddNewCategory = async (values) => {
+    if (!isValid) return;
+    values.slug = slugify(values.slug || values.name);
+    values.status = Number(values.status);
+    const colRef = collection(db, "categories");
+    try {
+      await addDoc(colRef, {
+        ...values,
+        createdAt: serverTimestamp(),
+      });
+      toast.success("Created new category successfully");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      reset({
+        name: "",
+        slug: "",
+        status: 1,
+        createdAt: new Date(),
+      });
+    }
   };
   return (
     <div>
@@ -48,16 +76,31 @@ const CategoryAddNew = () => {
           <Field>
             <Label>Status</Label>
             <div className="flex flex-wrap gap-x-5">
-              <Radio name="status" control={control} checked={true}>
+              <Radio
+                name="status"
+                control={control}
+                checked={Number(watchStatus) === categoryStatus.APPROVED}
+                value={1}
+              >
                 Approved
               </Radio>
-              <Radio name="status" control={control}>
+              <Radio
+                name="status"
+                control={control}
+                checked={Number(watchStatus) === categoryStatus.UNAPPROVED}
+                value={2}
+              >
                 Unapproved
               </Radio>
             </div>
           </Field>
         </div>
-        <Button type="submit" className="mx-auto">
+        <Button
+          type="submit"
+          className="mx-auto w-[230px]"
+          disabled={isSubmitting}
+          isLoading={isSubmitting}
+        >
           Add new category
         </Button>
       </form>
